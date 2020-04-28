ccab44623ccbca523bb5a14a4ee241f9d48460e6
Masaki Kozuki <masaki.kozuki.2014@gmail.com>
2020-04-27 18:49:07 -0700

    combine `found_inf` tensors from all devices

--- torch/optim/srsgd.py
+++ torch/optim/srsgd.py
@@ -1,6 +1,7 @@
 import torch
 from torch.cuda.amp.grad_scaler import _MultiDeviceReplicator
 from .sgd import SGD
+from ._amp_helper import _combined_found_inf_helper
 
 
 class SRSGD(SGD):
@@ -36,13 +37,12 @@ class SRSGD(SGD):
                 loss = closure()
 
         if grad_scaler is not None:
-            found_inf = grad_scaler._check_inf_per_device(self)
-            scale = grad_scaler._get_scale_async()
-            inv_scale = scale.double().reciprocal().float()
+            inv_scale = grad_scaler._get_scale_async().double().reciprocal().float()
+            found_inf = _combined_found_inf_helper(self, grad_scaler, inv_scale.device)
         else:
-            found_inf = torch.zeros((1,), dtype=torch.float, device=torch.cuda.current_device())
-            found_inf = _MultiDeviceReplicator(found_inf)
             inv_scale = torch.ones((1,), dtype=torch.float, device=torch.cuda.current_device())
+            found_inf = _MultiDeviceReplicator(
+                torch.zeros((1,), dtype=torch.float, device=torch.cuda.current_device()))
 
         inv_scale = _MultiDeviceReplicator(inv_scale)
 
